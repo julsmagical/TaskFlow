b475de2f2fdb88fbf83a2c0335efb853b1ac3ac3
Juliet <julietcaro40@gmail.com>
2026-06-19 09:44:25 -0500

    feat: base del backend

---
+++ backend/src/test/java/com/springboot/taskflow/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.taskflow.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
