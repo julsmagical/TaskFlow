d35fa397ba8c364a6420fe7b85577c943dc08978
Juliet <julietcaro40@gmail.com>
2026-06-19 13:43:35 -0500

    feat: entidad y repositorio de rol

--- backend/src/main/java/com/springboot/taskflow/taskflow/entities/Role.java
+++ backend/src/main/java/com/springboot/taskflow/taskflow/entities/Role.java
@@ -1,5 +1,44 @@
 package com.springboot.taskflow.taskflow.entities;
 
-public class Role {
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role(){}
+    
+    public Role(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
---
+++ backend/src/main/java/com/springboot/taskflow/taskflow/repositories/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.springboot.taskflow.taskflow.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springboot.taskflow.taskflow.entities.Role;
+
+public interface RoleRepository extends CrudRepository<Role, UUID>{
+
+    Optional<Role> findById(UUID id);
+
+    Optional<Role> findByName(String name);
+}
