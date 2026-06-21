994703e42fdbe326b88b29277c3673a328a2c2f6
Juliet <julietcaro40@gmail.com>
2026-06-21 13:48:37 -0500

    feat: userService para encontrar a todos los desarrolladores

---
+++ frontend/src/app/features/services/private/user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+import { map, Observable } from 'rxjs';
+import { environment } from '../../../../environment/environment';
+import { ApiResponse } from '../../interfaces/private/api-response.interface';
+import { UserRole } from '../../../shared/enums/user';
+import { SelectableUser } from '../../interfaces/public/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+    private readonly http = inject(HttpClient);
+    private readonly apiUrl = `${environment.apiUrl}/users`;
+
+    findAll(role?: UserRole): Observable<SelectableUser[]>{
+        let params = new HttpParams();
+
+        if(role){
+            params = params.set('role', role);
+        }
+
+        return this.http
+            .get<ApiResponse<SelectableUser[]>>(this.apiUrl, {params})
+            .pipe(map((response) => response.data));
+    }
+
+    findDevelopers(): Observable<SelectableUser[]>{
+        return this.findAll(UserRole.DESARROLLADOR);
+    }
+}
